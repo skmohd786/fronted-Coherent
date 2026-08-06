976cabf55dc207b53ec8132e2c640af79df01b2b
shaikh mohd <shaikhmohd00009@gmail.com>
2026-08-06 19:21:13 +0530

    changes to button

--- src/Components/Premium.jsx
+++ src/Components/Premium.jsx
@@ -78,7 +78,7 @@ const Premium = () => {
       <p className="mt-2 text-gray-400">Enjoy all your premium features.</p>
       <button
         className="btn btn-primary mt-8"
-        onClick={() => navigate("/feed")}
+        onClick={() => navigate("/")}
       >
         Go to Feed
       </button>
