1aac3f25d4f34bdf422b2feaca81a01babe2485f
shaikh mohd <shaikhmohd00009@gmail.com>
2026-08-06 18:53:44 +0530

    some updation on premium page

--- src/Components/Premium.jsx
+++ src/Components/Premium.jsx
@@ -1,36 +1,34 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { BASE_URL } from "../utils/constants";
 import axios from "axios";
 
 const Premium = () => {
+  const [isPremium, setIsPremium] = useState(false);
 
-  const[isPremium, setIsPremium] = useState(false);
-
-  const verifyPremiumUser = async (response) => {
-  try {
-    const res = await axios.post(
-      BASE_URL + "/payment/verify",
-      response,
-      {
+  const verifyPremiumUser = async () => {
+    try {
+      const res = await axios.get(BASE_URL + "/payment/verify", {
         withCredentials: true,
-      }
-    );
+      });
 
-    if (res.data.isPremium) {
-      setIsPremium(true);
-      alert("Payment successful! You are now a premium member.");
+      if (res.data.isPremium) {
+        setIsPremium(true);
+      }
+    } catch (err) {
+      console.error(err);
     }
-  } catch (err) {
-    console.error(err);
-    alert("Payment verification failed.");
-  }
-};
+  };
+
+  useEffect(() => {
+    verifyPremiumUser();
+  }, []);
+
   const handleBuyClick = async (membershipType) => {
     try {
       const response = await axios.post(
         BASE_URL + "/payment/create",
         { membershipType },
-        { withCredentials: true },
+        { withCredentials: true }
       );
 
       const { keyId, order } = response.data;
@@ -49,7 +47,12 @@ const Premium = () => {
         theme: {
           color: "#F37254",
         },
-        handler: verifyPremiumUser,
+        handler: () => {
+          setTimeout(async () => {
+            await verifyPremiumUser();
+            alert("Payment successful! Please wait while we activate your membership.");
+          }, 3000);
+        },
       };
 
       const rzp = new window.Razorpay(options);
@@ -59,7 +62,8 @@ const Premium = () => {
       alert("Unable to initiate payment. Please try again.");
     }
   };
-  return isPremium? (
+
+  return isPremium ? (
     <h1>You're already a premium member!</h1>
   ) : (
     <div className="m-6 p-5">
@@ -69,7 +73,7 @@ const Premium = () => {
           <ul>
             <li>- Unlimited Chats with connections</li>
             <li>- Send 100 Connection Requests per day</li>
-            <li>- Silver Verified Badge </li>
+            <li>- Silver Verified Badge</li>
             <li>- Ad-free Experience</li>
             <li>- 2 months validity</li>
           </ul>
@@ -80,7 +84,9 @@ const Premium = () => {
             Buy Silver
           </button>
         </div>
+
         <div className="divider divider-horizontal">OR</div>
+
         <div className="card bg-base-300 rounded-box grid h-80 grow place-items-center">
           <h1 className="text-3xl font-bold">Gold Membership</h1>
           <ul>
