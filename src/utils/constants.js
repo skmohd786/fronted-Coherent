b28d32c4bb3bee042d4e25dd43483e40252155bd
shaikh mohd <shaikhmohd00009@gmail.com>
2026-09-07 03:23:45 +0530

    fix production backend url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,6 @@
-export const BASE_URL = location.hostname === "localhost" ? "http://localhost:7777" : "/api";
+export const BASE_URL =
+  location.hostname === "localhost"
+    ? "http://localhost:7777"
+    : "https://backend-coherent.onrender.com";
+
 export const DEFAULT_PROFILE_IMAGE = "/Coherent-logo.png";
